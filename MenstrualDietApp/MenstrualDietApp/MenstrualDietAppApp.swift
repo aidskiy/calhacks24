8c438aff3894ec9339b25556e733a0f2927a9b1c
Aida Bozulan <49568146+aidskiy@users.noreply.github.com>
2024-10-19 11:42:52 -0700

    mobile app starter code

---
+++ MenstrualDietApp/MenstrualDietApp/MenstrualDietAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenstrualDietAppApp.swift
+//  MenstrualDietApp
+//
+//  Created by Nayla Sese on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct MenstrualDietAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
